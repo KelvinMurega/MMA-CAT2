a692d123ef19f3268cb0bacd6624741ca4bfef5a
KelvinMurega <kevomurega@gmail.com>
2026-06-26 15:47:57 +0300

    Completed multimedia wildlife webpage assignment

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const video = document.getElementById("wildlifeVideo");
-
 const button = document.getElementById("toggleButton");
 
 button.addEventListener("click", () => {
@@ -7,20 +6,16 @@ button.addEventListener("click", () => {
     if (video.style.display === "none") {
 
         video.style.display = "block";
-
         video.play();
 
-    }
+        button.textContent = "🌿 Hide Video";
 
-    else {
+    } else {
 
-        if (video.paused) {
-
-            video.play();
-
-        }
-
+        video.pause();
         video.style.display = "none";
+
+        button.textContent = "▶ Show & Play Video";
 
     }
 
